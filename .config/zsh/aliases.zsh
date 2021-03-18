a46f3b570f421b36265f9427f03e61e405486de1
Thiago Arroadie Costa <thiago@arroadie.com>
2021-03-17 17:50:50 -0700

    add config folder

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,13 @@
+
+alias cat=ccat
+
+# Colorize `grep`s
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias mkdir='mkdir -pv'
+alias tree='tree -C'
+
+alias l='k -h'
+alias ls='ls --color=always --group-directories-first'
---
+++ .config/zsh/functions.zsh
@@ -0,0 +1,6 @@
+turbo0()   { zinit ice wait"0a" lucid             "${@}"; }
+turbo1()   { zinit ice wait"0b" lucid             "${@}"; }
+turbo2()   { zinit ice wait"0c" lucid             "${@}"; }
+zcommand() { zinit ice wait"0b" lucid as"command" "${@}"; }
+zload()    { zinit load                           "${@}"; }
+zsnippet() { zinit snippet                        "${@}"; }
